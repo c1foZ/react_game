7ac0d25997fe82dcfca08adafc45c6fa4a62ddbb
c1foZ <tomife2@gmail.com>
2024-04-11 20:45:08 +0200

    added hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { ReactTyped } from 'react-typed';
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+        <p className='text-[#ff893b] font-bold p-3'>GROWING WITH DATA ANALYTICS</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-3'>Grow with data.</h1>
+        <div className='flex justify-center items-center md:pt-3'>
+          <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing</p>
+          <ReactTyped className='md:text-5xl sm:text-4xl text-xl font-bold pl-2'
+            strings={['BTB', 'BTC', 'HEYS']}
+            typeSpeed={400} 
+            backSpeed={400}
+            loop 
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
