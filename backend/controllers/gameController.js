a7db191903f016933fe465a751b07d39f4c41dc8
c1foZ <tomife2@gmail.com>
2024-04-16 14:32:34 +0200

    creating game controller

---
+++ backend/controllers/gameController.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const Game = require('../models/gameModel.js')
+
+const createGame = async (req, res) => {
+    const {title, players, description} = req.body
+
+    try {
+        const game = await Game.create({title, players, description} )
+        res.status(200).json(game)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+const getAllGames = async (req, res) => {
+    try {
+        const games = await Game.find();
+        res.status(200).json(games);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const getGame = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const game = await Game.findById(id);
+        if (!game) {
+            return res.status(404).json({ message: 'Game not found' });
+        }
+        res.status(200).json(game);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const deleteGame = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const game = await Game.findById(id);
+        if (!game) {
+            return res.status(404).json({ message: 'Game not found' });
+        }
+        
+        await Game.findByIdAndDelete(id);
+        res.status(200).json({ message: 'Game deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = {
+    createGame,
+    getAllGames,
+    getGame,
+    deleteGame
+}
--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,56 +1,21 @@
 const express = require('express')
 const Game = require('../models/gameModel.js')
+const {
+    createGame,
+    getAllGames,
+    getGame,
+    deleteGame
+} = require('../controllers/gameController.js')
 
 const router = express();
 
-router.get('/', async (req, res) => {
-    try {
-        const games = await Game.find();
-        res.status(200).json(games);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-})
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
+router.get('/', getAllGames)
+router.get('/:id', getGame)
+router.post('/', createGame)
+router.delete('/:id', deleteGame)
 
-    try {
-        const game = await Game.findById(id);
-        if (!game) {
-            return res.status(404).json({ message: 'Game not found' });
-        }
-        res.status(200).json(game);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-})
-router.post('/', async (req, res) => {
-    const {title, players, description} = req.body
-
-    try {
-        const game = await Game.create({title, players, description} )
-        res.status(200).json(game)
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-})
 router.put('/:id', (req, res) => {
     res.json({mssg: 'Update single game!!'})
-})
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const game = await Game.findById(id);
-        if (!game) {
-            return res.status(404).json({ message: 'Game not found' });
-        }
-        
-        await Game.findByIdAndDelete(id);
-        res.status(200).json({ message: 'Game deleted successfully' });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
 })
 
 module.exports = router
